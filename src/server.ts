a83eeb0357001f020c405308eb13b045db58535d
Ridwan Abiola <ricklick262@gmail.com>
2023-09-22 23:04:50 +0100

    feat: telegraf bot base setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
 app.listen(port, function () {
     console.log(`Express Server initiated listening on port ${port}`);
