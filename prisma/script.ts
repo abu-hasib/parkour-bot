7981d9ea3671fd5052585a08c894c1d48e3b7725
Ridwan Abiola <ricklick262@gmail.com>
2023-09-23 23:02:26 +0100

    fix: migrations

--- prisma/script.ts
+++ prisma/script.ts
@@ -4,14 +4,17 @@ import prisma from "../src/lib/prisma"
 async function main() {
   // ... you will write your Prisma Client queries here
   const data: Prisma.UserCreateInput = {
-    name: "Bob",
-    email: "bob@prisma.io",
+    name: "Love",
+    email: "love@prisma.io",
     posts: {
       create: {
         title: "Hello World",
       },
     },
   };
+  // const user  = await prisma.user.create({
+  //   data
+  // })
   const usersWithPosts = await prisma.user.findMany({
     include: {
       posts: true,
